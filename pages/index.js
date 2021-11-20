180c8378eebcc0403a80e0f6b863536418931bf0
Anton Plauche <antonplauche92@gmail.com>
2021-11-20 11:16:33 -0600

    clean up

--- pages/index.js
+++ pages/index.js
@@ -125,11 +125,12 @@ export async function getStaticProps() {
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget leo sed magna pulvinar scelerisque quis ac lacus. Nullam eu auctor arcu. Vivamus eu facilisis orci. Cras massa nunc, consequat nec egestas non, maximus in massa. Proin a dictum sem, id tempor leo. Phasellus a bibendum dolor. Ut gravida rhoncus risus, ut aliquam odio dapibus id. Aliquam at semper neque. Vivamus quis neque a velit commodo iaculis vitae quis leo.',
       },
   
-]}
+    ]
+  }
 
   return {
     props: {
       data
-    }, // will be passed to the page component as props
+    },
   }
 }
